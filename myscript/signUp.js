cbd1c8ebe5f4ddaf95644d86bfa13829ae28067d
lymeansreang <lmsreang3@gmail.com>
2023-12-15 18:59:47 +0700

    change signUp page from fristName lastName to username

--- myscript/signUp.js
+++ myscript/signUp.js
@@ -1,41 +1,45 @@
 function handleSignUpClick() {
-    const firstName = document.getElementById('firstName').value.trim();
-    const lastName = document.getElementById('lastName').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const password = document.getElementById('password').value.trim();
-    const rememberMe = document.getElementById('remember').checked;
-    //catch all data that input
-    const userData = {
-      firstName,
-      lastName,
-      email,
-      password,
-      rememberMe,
-    };
-    console.log(userData)
-    window.location.href = '/src/index.html';
-    // fetch('/jsondata/signup.json', {
-    //   method: 'GET',
-    // //   headers: {
-    // //     'Content-Type': 'application/json',
-    // //   },
-    // //   body: JSON.stringify(userData),
-    // })
-    //   .then(response => {
-    //     if (!response.ok) {
-    //       const errorMessage = response.statusText || 'Unknown error';
-    //       throw new Error(errorMessage);
-    //     }
-    //     return response.json();
-    //   })
-    //   .then(data => {
-    //     if (data.userSignUp && data.userSignUp.length > 0) {
-    //       alert('Account created! Please check your email for verification.');
-    //       window.location.href = '/src/index.html';
-    //     } else {
-    //       alert(data.message || 'Signup failed. Please try again');
-    //     }
-    //   })
-    //   .catch(error => console.error('Error:', error));
-  }
-  
+  const username = document.getElementById("username").value
+  const email = document.getElementById("email").value
+  const password = document.getElementById("password").value
+
+  const userData = {
+    username,
+    email,
+    password,
+  };
+  fetch("https://cms.istad.co/api/auth/local/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  })
+    .then((response) => {
+      console.log("Response:", response);
+      if (!response.ok) {
+        // Check if the response contains JSON
+        return response.json().then((errorData) => {
+          const errorMessage = errorData.message || "Unknown error";
+          throw new Error(errorMessage);
+        });
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Data:", data);
+      if (data.user) {
+        alert("Account created! Please check your email for verification.");
+        //console.log(userData);
+        setTimeout(() => {
+          window.location.href = "/src/index.html";
+        }, 1000);
+      } else {
+        alert(data.message || "Signup failed. Please try again");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error.message);
+      alert("Signup failed. Please try again.");
+    });
+}
